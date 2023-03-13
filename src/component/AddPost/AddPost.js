c29619734e2ed6926773f032a990a66fc646ca3b
rahulvijay81 <rahulvjay8156@gmail.com>
2023-03-13 22:31:51 +0530

    images uploadong firebase config

--- src/component/AddPost/AddPost.js
+++ src/component/AddPost/AddPost.js
@@ -1,21 +1,44 @@
 import { Fragment, React } from 'react'
 import '../../Style.scss'
+import { useState, useContext } from 'react'
+import { FirebaseContext, AuthContext } from '../../store/Context'
+
 
 function AddPost() {
+  const { firebase } = useContext(FirebaseContext)
+  const { user } = useContext(AuthContext)
+  const [image, setImage] = useState(null)
+  const date = new Date()
+
+  const handleSubmit = () => {
+    firebase.storage().ref(`/image/${image.name}`)
+      .put(image).then(({ ref }) => {
+        ref.getDownloadURL().then((url) => {
+          console.log(url);
+          firebase.firestore().collection('products').add({
+            url,
+            userId: user.uid,
+            createAt: date.toDateString()
+          })
+          alert('uploaded')
+        })
+      })
+  }
+
   return (
     <Fragment>
       <div className='postParentDiv'>
         <div className='postChildDiv'>
           <div className="profile">
-            <h6>User Profile</h6>
-            <div className='profileimg' />
-            <h3>rahul vijay</h3>
+            <h6>Upload Images</h6>
             <br />
             <p>share photos to your friends</p>
             <br />
-            <input type="file" />
+            <input onChange={(e) => {
+              setImage(e.target.files[0])
+            }} type="file" />
             <br />
-            <button className='imgupload'>Upload</button>
+            <button onClick={handleSubmit} className='imgupload'>Upload</button>
           </div>
         </div>
       </div>
--- src/component/Post/Posts.js
+++ src/component/Post/Posts.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../../Style.scss'
+// import { useState, useContext, useEffect } from 'react'
+// import { FirebaseContext } from '../../store/Context'
+// import { PostContext } from '../../store/PostContext'
+// import { useNavigate } from 'react-router-dom'
 import imgcheck from '../../store/Image_created_with_a_mobile_phone.png.webp'
 
 function Post() {
@@ -13,7 +17,6 @@ function Post() {
             <p>04/03/2023</p>
           </div>
         </div>
-        
 
         <div className="box"></div>
 
