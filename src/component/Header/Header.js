e0f8ae92d29585544737dfb9e1297a33929feac3
rahulvijay81 <rahulvjay8156@gmail.com>
2023-03-13 19:49:32 +0530

    logout config

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import '../../Style.scss'
+import { useContext } from 'react'
+import { AuthContext, FirebaseContext } from '../../store/Context'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const { user } = useContext(AuthContext)
+  const { firebase } = useContext(FirebaseContext)
+  const navigate = useNavigate()
+
   return (
     <div className='headerParentDiv'>
       <div className='headerChildDiv'>
@@ -15,8 +22,12 @@ function Header() {
           />
         </div>
         <div className='logout'>
-          <button className='logoutbutton'>
-            Log Out</button>
+          {user && <button onClick={() => {
+            firebase.auth().signOut();
+            navigate('/login')
+          }} className='logoutbutton'>Log Out</button>
+
+          }
 
         </div>
       </div>
