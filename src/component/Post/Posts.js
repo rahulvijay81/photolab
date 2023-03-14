5d4a8918b635837b9c907255d511a0e793bb957b
rahulvijay81 <rahulvjay8156@gmail.com>
2023-03-14 12:46:26 +0530

    uploading image

--- src/component/Post/Posts.js
+++ src/component/Post/Posts.js
@@ -1,28 +1,46 @@
 import React from 'react'
 import '../../Style.scss'
-// import { useState, useContext, useEffect } from 'react'
-// import { FirebaseContext } from '../../store/Context'
-// import { PostContext } from '../../store/PostContext'
-// import { useNavigate } from 'react-router-dom'
-import imgcheck from '../../store/Image_created_with_a_mobile_phone.png.webp'
+import { useState, useContext, useEffect } from 'react'
+import { FirebaseContext } from '../../store/Context'
+import { PostContext } from '../../store/PostContext'
+import { useNavigate } from 'react-router-dom'
 
 function Post() {
+  const [products, setProducts] = useState([])
+  const { firebase } = useContext(FirebaseContext)
+  const { setPostDetails } = useContext(PostContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    firebase.firestore().collection('products').get().then((snapshot) => {
+      const allPost = snapshot.docs.map((product) => {
+        return {
+          ...product.data(),
+          id: product.id
+        }
+      })
+      setProducts(allPost)
+    })
+  })
+
   return (
     <div>
-      <div className="postarea">
-        <div className="box">
-          <div className='postimages'>
-            <img src={imgcheck} alt="" />
-            <p>Post by Rahul Vijay</p>
-            <p>04/03/2023</p>
+      {products.map(product => {
+        return <div className="postarea">
+          <div className="box">
+            <div className='postimages' onClick={() => {
+              setPostDetails(product)
+              navigate('/')
+            }}>
+              <img src={product.url} alt="" />
+            </div>
           </div>
         </div>
+      })
+      }
 
-        <div className="box"></div>
-
-      </div>
     </div>
-  )
+  );
 }
 
 export default Post
